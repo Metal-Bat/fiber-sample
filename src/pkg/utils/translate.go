db4b4d07b3c750f431bd3645a9ab2c74f6d839ca
Erfan <erfan.ehsany@outlook.com>
2026-06-05 20:34:08 +0330

    build: updated go lang

--- src/pkg/utils/translate.go
+++ src/pkg/utils/translate.go
@@ -3,7 +3,7 @@ package utils
 import (
 	"sample/src/initializers"
 
-	contribI18nMiddleware "github.com/gofiber/contrib/v3/i18n"
+	"github.com/gofiber/contrib/v3/i18n"
 	"github.com/gofiber/fiber/v3"
 )
 
@@ -11,7 +11,7 @@ func TranslateMessage(c fiber.Ctx, message string) (string, error) {
 	_, span := initializers.Tracer.Start(c.Context(), "utils.TranslateMessage")
 	defer span.End()
 
-	translatedMessage, err := contribI18nMiddleware.Localize(c, message)
+	translatedMessage, err := i18n.Localize(c, message)
 	if err != nil {
 		span.RecordError(err)
 		return message, nil
